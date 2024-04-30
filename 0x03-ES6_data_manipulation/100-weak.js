55da53d71933c124ae975b977faf9ded982df206
Colightech <saintchris4@gmail.com>
2024-04-30 07:27:33 +0000

    Track within the weakMap the number of times queryAPI is called for each endpoint. When the number of queries is >= 5 throw an error with the message Endpoint load is high

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,6 +1,3 @@
-/*Track within the weakMap the number of times queryAPI is called for each endpoint.
-
-When the number of queries is >= 5 throw an error with the message Endpoint load is high.*/
 export const weakMap = new WeakMap();
 
 export const queryAPI = (endpoint) => {
